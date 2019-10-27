2e3b3f84cc0e6ab99d2fcd289af6385cd5b7445a
Dean McCluskey <dean1250@gmail.com>
2019-10-26 21:41:46 -0700

    update script

--- assets/script.js
+++ assets/script.js
@@ -47,6 +47,51 @@ $(document).ready(function () {
         }
     });
 
+    // Add click event listener to all animal buttons to display the appropriate content
+    $(document).on("click", "button", function () {
+        // Grab, store data-animal property value from button
+        var animal = $(this).attr("data-animal");
+        // Construct queryURL using animal name
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal +
+            "&api_key=9iCtnbt3iMLZtI9xANJQ1k4IdAT2jegT";
+
+        // Perform AJAX request with queryURL
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            // After data comes back from the request
+            .then(function (response) {
+                console.log(response);
+                // Store data from AJAX request in results variable
+                var results = response.data;
+                // Loop through result items
+                for (let i = 0; i < results.length; i++) {
+                    // Create, store div tag
+                    var animalDiv = $("<div>");
+                    // Create, store image tag, and save still, moving images pulled off result item
+                    var animalImage = $("<img>");
+                    var movingImage = results[i].images.fixed_height.url;
+                    var stillImage = results[i].images.fixed_height_still.url;
+                    // Assign moving, still attributes to image tag
+                    animalImage.attr("data-moving", movingImage);
+                    animalImage.attr("data-still", stillImage);
+                    // Set src attribute of still image to image tag 
+                    animalImage.attr("src", stillImage);
+                    // Create paragraph tag with the result item's title
+                    var title = $("<p>").text("Title:  " + results[i].title);
+                    // Append image tag to the animalDiv
+                    animalDiv.append(animalImage);
+                    // Append title paragragh to animalDiv, line break
+                    animalDiv.append(title);
+                    animalDiv.append("<br>");
+                    // Prepend animalDiv to the HTML page in the "#animal-gifs" div
+                    // Sends previous gifs to end of page
+                    $("#animal-gifs").prepend(animalDiv);
+                }
+            });
+    });
+
     // Add click event listener to each image tag to toggle still to moving gif
     $(document).on("click", "img", function () {
         var imgTag = $(this);
@@ -60,48 +105,6 @@ $(document).ready(function () {
         }
     });
 
-    // Add click event listener to all animal buttons to display the appropriate content
-    $(document).on("click", "button", function () {
-        // Grab, store data-animal property value from button
-        var animal = $(this).attr("data-animal");
-        // Construct queryURL using animal name
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal +
-            "&api_key=9iCtnbt3iMLZtI9xANJQ1k4IdAT2jegT";
-        // Perform AJAX request with queryURL
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
-            // After data comes back from the request
-            .then(function (response) {
-                console.log(response);
-                // Store data from AJAX request in results variable
-                var results = response.data;
-                // Loop through 10 result items
-                for (let i = 0; i < 10; i++) {
-                    // Create, store div tag
-                    var animalDiv = $("<div>");
-                    // Create, store image tag, and still, moving images pulled off result item
-                    var animalImage = $("<img>");
-                    var movingImage = results[i].images.fixed_height.url;
-                    var stillImage = results[i].images.fixed_height_still.url;
-                    // Assign moving, still attributes to image tag
-                    animalImage.attr("data-moving", movingImage);
-                    animalImage.attr("data-still", stillImage);
-                    // Set src attribute of still image to image tag 
-                    animalImage.attr("src", stillImage);
-                    // Create paragraph tag with the result item's title
-                    var title = $("<p>").text("Title:  " + results[i].title);
-                    // Append image tag to the animalDiv
-                    animalDiv.append(animalImage);
-                    // Append title to animalDiv
-                    animalDiv.append(title);
-                    animalDiv.append("<br>");
-                    // Prepend animalDiv to the HTML page in the "#animal-gifs" div
-                    $("#animal-gifs").prepend(animalDiv);
-                }
-            });
-    });
 });
 
 // Copyright 2019 Dean A McCluskey
